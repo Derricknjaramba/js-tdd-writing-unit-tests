fd58e9f0762d69d27c71ab5e4c7a29b850f3d261
Derricknjaramba <njarambaderrick@gmail.com>
2024-08-10 11:08:17 +0300

    unit

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,17 @@
-// Your code here
-// src/utils.js
 export function pointsForWord(word) {
-    let points = 0;
-    for (const char of word) {
-      if (["a", "e", "i", "o", "u"].includes(char)) {
-        points += 1;
-      } else {
-        points += 2;
-      }
-    }
-    return points;
+  if (typeof word !== 'string') {
+    throw new Error('Input must be a string');
   }
+  if (!/^[A-Za-z]*$/.test(word)) {
+    throw new Error('Input must only contain alphabetic characters');
+  }
+  
+  let points = 0;
+  for (const char of word) {
+    points += /[aeiou]/i.test(char) ? 1 : 2;
+  }
+  return points;
+}
+
   
   
